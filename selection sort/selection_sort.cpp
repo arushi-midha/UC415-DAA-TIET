2b45a5759f040378359d1480d29f6ac1121aacc4
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2024-04-17 12:23:53 +0530

    selection sort for some reason

---
+++ selection sort/selection_sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of elements in array ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+    int min;
+    for(int i=0;i<n;i++){
+        min=i;
+        for(int j=i+1;j<n;j++){
+            //finding the minimum element in the unsorted array
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        //swap minimum element for the first unsorted element
+        int temp=arr[i];
+        arr[i]=arr[min];
+        arr[min]=temp;
+    }
+
+    cout<<"Sorted array is: ";
+    for(int i=0;i<n;i++){
+
+        cout<<arr[i]<<" ";
+    }
+}
